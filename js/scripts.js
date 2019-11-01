7d2e4afe90d2bd927967b5dad7efe29d6451ff23
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-11-01 12:33:45 -0700

    add for loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,12 @@ $(document).ready(function(){
   $("#beepForm").submit(function(event){
     event.preventDefault();
     var userInput = parseInt($(".beepNum").val())
-    console.log("test")
     var userInputArr=[];
-    console.log("test1")
     function userInputSplit (userInput){
       var userInputPush = userInput
+      for(i=1; i<= userInput; i++){
+        userInputArr.push(userInputPush)
+      }
 
     }
 
