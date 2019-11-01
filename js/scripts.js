866344413de191dec6022dbbc376d8c0e99c0f56
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-11-01 13:21:10 -0700

    add more console.logs for debugging

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
-  $("#beepForm").submit(function(event){
+  $("#beepForm").submit(function(){
     event.preventDefault();
     var userInput = parseInt($("input#beepNum").val());
     console.log(typeof userInput)
     var userInputArr=[];
-    var userNum1 = function(){
+    var userSplit = function(){
+      console.log("test")
       var userInputPush = userInput;
+      console.log(userInput)
       for(i=1; i <= userInput; i++){
       userInputArr.push(userInputPush)
       userInputPush -= 1;
-
       }
+      // for (i = 0; i < userInputArr.length; i ++){
+      //   if(userInputArr[i].toString().inclueds(3)){
+      //     userInputArr[i] = 'boop';
+      //   }
+      // }
     };
-
-    
-
-
-
   });
 });
