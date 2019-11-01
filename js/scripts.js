21cfed97f2cb4333c6597c6f4ea57450bfd6f0cc
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-11-01 11:04:29 -0700

    take input from the form and change it into a number

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
   $("#beepForm").submit(function(event){
+    event.preventDefault();
     var userInput = parseint($(".beepNum").val())
-  });
+    var userInputArr=[];
+    function userInputSplit (userInput){
+      var userInputPush = userInput
+      
+    }
 
+
+
+  });
 });
